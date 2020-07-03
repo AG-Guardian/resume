387e02cc40f8be73c02c798beca3161a85948935
AG-Guardian <zachary.a.greer@gmail.com>
2020-07-02 22:59:18 -0500

    more refactoring for firestore and auth promise chaining

--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
 // TODO: add recaptcha auth https://firebase.googleblog.com/2017/08/guard-your-web-content-from-abuse-with.html
 
-firebase.auth().signInAnonymously().catch(function(error) {
+firebase.auth().signInAnonymously().then( ()=> {
+  let db = firebase.firestore();
+
+  let docRef = db.collection("visitors").doc("count");
+
+  docRef.get().then(function(doc) {
+    let n;
+    if (doc.exists) {
+      n = doc.data()['value'];
+
+      if (n === null) {
+        n = 0;
+      }
+      n++;
+
+    } else {
+      n = 1;
+    }
+
+    db.collection("visitors").doc("count").set({
+      value: n
+    });
+
+    nums = n.toString().split('').map(Number);
+    fill = [];
+    for (let i = 0; i < 6 - nums.length; i++) {
+      fill.push(0);
+    }
+    nums = fill.concat(nums);
+
+    document.getElementById('views').innerHTML = '<b>Views: </b>';
+    document.getElementById('counter').innerHTML = '';
+    for (let i of nums) {
+      document.getElementById('counter').innerHTML += '<span class="counter-item">' + i + '</span>';
+    }
+
+  }).catch(function(error) {
+    console.log("Error getting document:", error);
+  });
+}).catch(function(error) {
   let errorCode = error.code;
   let errorMessage = error.message;
-});
-
-let db = firebase.firestore();
-
-let docRef = db.collection("visitors").doc("count");
-
-docRef.get().then(function(doc) {
-  let n;
-  if (doc.exists) {
-    n = doc.data()['value'];
-
-    if (n === null) {
-      n = 0;
-    }
-    n++;
-
-  } else {
-    n = 1;
-  }
-
-  db.collection("visitors").doc("count").set({
-    value: n
-  });
-
-  nums = n.toString().split('').map(Number);
-  fill = [];
-  for (let i = 0; i < 6 - nums.length; i++) {
-    fill.push(0);
-  }
-  nums = fill.concat(nums);
-
-  document.getElementById('counter').innerHTML = '';
-  for (let i of nums) {
-    document.getElementById('counter').innerHTML += '<span class="counter-item">' + i + '</span>';
-  }
-
-}).catch(function(error) {
-  document.getElementById('views').innerText = '';
-  console.log("Error getting document:", error);
 });
